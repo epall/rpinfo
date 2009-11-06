26730bfcb478f134e7015d499944e15968aa657c
Eric Allen <ericpallen@gmail.com>
2009-11-06 09:25:06 -0500

    Successful email parsing

--- rpinfo.rb
+++ rpinfo.rb
@@ -4,15 +4,37 @@ require 'open-uri'
 require 'uri'
 require 'nokogiri'
 
-#1: Simple POST
-res = Net::HTTP.post_form(URI.parse('http://prod3.server.rpi.edu/peopledirectory/search.do'),
-                          {'query'=>'Eric Allen', 'datasetName' => 'directory', 'qct' => '10'})
-doc = Nokogiri::HTML(open(res['location']))
-people = doc.css('td.listingName a')
-top_hit = people[0]
+class Person
+  attr_reader :name, :email
 
-person_page = Nokogiri::HTML(open('http://prod3.server.rpi.edu/peopledirectory/'+top_hit.attribute('href')))
-email_glyphs = person_page.css('td.email img')
-email_glyphs.each do |glyph|
-  puts glyph['src'].split('/')[10].split('.')[0].to_i
+  def initialize(name)
+    @name = name
+    res = Net::HTTP.post_form(URI.parse('http://prod3.server.rpi.edu/peopledirectory/search.do'),
+                        {'query'=>name.gsub(' ', ' AND '), 'datasetName' => 'directory', 'qct' => '10'})
+    doc = Nokogiri::HTML(open(res['location']))
+    people = doc.css('td.listingName a')
+    top_hit = people[0]
+    person_url = 'http://prod3.server.rpi.edu/peopledirectory/'+top_hit.attribute('href')
+
+    person_page = Nokogiri::HTML(open(person_url))
+    email_glyphs = person_page.css('td.email img')
+    @email = ''
+    email_glyphs.each do |glyph|
+      value = glyph['src'].split('/')[10].split('.')[0].to_i
+      case value
+      when 1..27
+        @email << (value + 'a'[0] - 1).chr
+      when 28..36
+        @email << (value-28 + '1'[0]).chr
+      when 37
+        @email << '0'
+      when 99
+        @email << '@rpi.edu'
+      else
+        @email << '?'
+      end
+    end
+  end
 end
+
+puts Person.new(ARGV[0]).email
